3ae0def0fbf6707b6ec1dce5161fc97897ced97d
Holtz Yan <yan.holtz@datadoghq.com>
2022-01-19 10:24:02 -0400

    add covid19 India

--- inspiration/inspiration-list.js
+++ inspiration/inspiration-list.js
@@ -6,9 +6,23 @@ const inspirationList = [
         img: "omicron_NYT.jpeg",
         description: "We're in early 2022, the Omicron variant importance is raising across the globe and the New York Times <a href='https://twitter.com/FreedZach/status/1479132573578240001'>broke the internet</a> with a circular streamgraph.",
         tool: undefined,
+        code: undefined,
         family: "evolution",
         type: "streamchart",
-        luminosity: "light",
+        luminosity: ["light"],
         interactive: false
+    },
+    {
+        title: "Covid 19 Dashboard - India",
+        author: "Group of dedicated volunteers",
+        url: "https://www.covid19india.org/about",
+        img: "covid19India",
+        description: "A dashboard describing the spread of Covid 19 in india. Highly interactive with a pretty slick design. Dark mode support, many different types of viz with smooth transition between dataset.",
+        tool: ["d3.js", "react"],
+        code: "https://github.com/covid19india/covid19india-react",
+        family: "dashboard",
+        type: undefined,
+        luminosity: ["light", "dark"],
+        interactive: true
     }
 ]
