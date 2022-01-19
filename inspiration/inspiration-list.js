e646c0c1fcdc557cb11f533db853c33c698e239a
Holtz Yan <yan.holtz@datadoghq.com>
2022-01-19 10:13:27 -0400

    Update inspiration-list.js

--- inspiration/inspiration-list.js
+++ inspiration/inspiration-list.js
@@ -3,8 +3,12 @@ const inspirationList = [
         title: "Here’s When We Expect Omicron to Peak",
         author: "The New York Times",
         url: "https://www.nytimes.com/2022/01/06/opinion/omicron-covid-us.html",
-        img: "",
-        description: "",
-        tool: ""
+        img: "omicron_NYT.jpeg",
+        description: "We're in early 2022, the Omicron variant importance is raising across the globe and the New York Times <a href='https://twitter.com/FreedZach/status/1479132573578240001'>broke the internet</a> with a circular streamgraph.",
+        tool: undefined,
+        family: "evolution",
+        type: "streamchart",
+        luminosity: "light",
+        interactive: false
     }
 ]
