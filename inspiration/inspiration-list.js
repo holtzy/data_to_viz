3d948da42465d8b5242beb48e86ff513270dde70
holtzy <yan.holtz@datadoghq.com>
2022-01-19 10:04:47 -0400

    e

--- inspiration/inspiration-list.js
+++ inspiration/inspiration-list.js
@@ -1,7 +1,8 @@
 const inspirationList = [
     {
-        title: "",
-        url: "",
+        title: "Here’s When We Expect Omicron to Peak",
+        author: "The New York Times",
+        url: "https://www.nytimes.com/2022/01/06/opinion/omicron-covid-us.html",
         img: "",
         description: "",
         tool: ""
